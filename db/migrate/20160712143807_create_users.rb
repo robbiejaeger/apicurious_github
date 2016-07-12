76d41b8b3c29774cf0a411d32c22d758687b1654
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-12 15:37:14 -0600

    user can login using github

--- db/migrate/20160712143807_create_users.rb
+++ db/migrate/20160712143807_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :uid
+      t.string :login
+      t.string :oauth_token
 
       t.timestamps
     end
