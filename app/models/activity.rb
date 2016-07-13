92276fa1c92f76e860f64437ff9654002cd24af0
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-13 13:21:17 -0600

    Add activity summary for user

---
+++ app/models/activity.rb
@@ -0,0 +1,15 @@
+class Activity < OpenStruct
+
+  def self.service
+    @@service ||= ActivityService.new
+  end
+
+  def self.user_activity(current_user)
+    user_events_hash = service.get_user_activity(current_user)
+
+    user_events_hash.map do |user_event_hash|
+      Activity.new(user_event_hash)
+    end
+  end
+
+end
